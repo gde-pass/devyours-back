afdc5503a3946009182078881f9742c6b1ce5707
Gregory <greg@coding-days.com>
2020-12-14 16:46:43 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c362da45dc4220ed414bdbdc8fbb4ae4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a7bf7ec6-4d59-4807-93b3-5276ad3157cd'
+};
